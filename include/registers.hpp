0902f08dddf0dc4cf4d87448f8219699767046ca
Seth Loh <lackdaz@gmail.com>
2021-03-12 16:31:09 +0800

    some live changes

--- include/registers.hpp
+++ include/registers.hpp
@@ -1,31 +1,16 @@
 #pragma once
 
 namespace jetsonbatt::registers {
-const unsigned int status = 0x00;
-const unsigned int current = 0x0A;
+const unsigned short int status = 0x00;
+const unsigned short int current = 0x0A;
+const unsigned int avg_current = 0x0B;
+const unsigned int minmax_current = 0x1C;
+const unsigned int avg_temp = 0x16;
+const unsigned int voltage = 0x09;
+const unsigned int minmax_voltage = 0x1B;
+const unsigned int state_of_charge = 0x06;
+const unsigned int cycles = 0x17;
+const unsigned int time_to_empty = 0x11;
+const unsigned int config2 = 0xBB;
+
 }  // namespace jetsonbatt::registers
-
-// _registers_d = {
-//     "status": 0x00,
-//     "current": 0x0A,
-//     "avg_current": 0x0B,
-//     "minmax_current": 0x1C,
-//     "avg_temp": 0x16,
-//     "voltage": 0x09,
-//     "minmaxvoltage": 0x1B,
-//     "state_of_charge": 0x06,
-//     "cycles": 0x17,
-//     "time_to_empty": 0x11,
-//     "config2": 0xBB,
-// }
-
-// serial_addresses = [
-//     0xDF,  # this is a free address, shows 0x00
-//     0xD4,
-//     0xD5,
-//     0xD9,
-//     0xDA,
-//     0xDC,
-//     0xDD,
-//     0xDE,
-// ]
--- test/test.cpp
+++ test/test.cpp
@@ -4,5 +4,7 @@
 
 int main() {
   printf("Hello there %#04x.\n", jetsonbatt::registers::current);
+  std::cout << "1) size of namespace variable:              "
+            << sizeof(jetsonbatt::registers::current) << "\n";
   return 0;
 }
